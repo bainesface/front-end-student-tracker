fd3b5802d6c3fc52ab66e944640b0ecbb410c5a6
Aniket <aniket.b@outlook.com>
2020-02-19 16:54:46 +0000

    working on add new student

--- student-tracker/src/components/AddNewStudent.jsx
+++ student-tracker/src/components/AddNewStudent.jsx
@@ -10,15 +10,37 @@ class AddNewStudent extends Component {
       <form>
         <h2>Add new student:</h2>
         <label>
-          Name: <input type="text" required onChange={this.handleChange} />
+          Name:{" "}
+          <input
+            type="text"
+            required
+            onChange={this.handleChange}
+            value={this.state.name}
+            name="name"
+          />
         </label>
         <label>
-          Starting Cohort: <input type="text" required />
+          Starting Cohort:{" "}
+          <input
+            type="text"
+            required
+            onChange={this.handleChange}
+            value={this.state.startingCohort}
+            name="startingCohort"
+          />
         </label>
         <button>Add student</button>
       </form>
     );
   }
+
+  handleChange = event => {
+    console.log(event.target.name, "event");
+    console.log(event.target.value);
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      console.log(this.state.name, this.state.startingCohort, "***");
+    });
+  };
 }
 
 export default AddNewStudent;
--- student-tracker/src/components/Header.jsx
+++ student-tracker/src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <header className="App-header">
-        <h1>Student Tracker</h1>
+        <h1>^Student Tracker</h1>
       </header>
     );
   }
